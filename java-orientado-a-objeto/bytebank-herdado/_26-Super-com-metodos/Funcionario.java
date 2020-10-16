d3642979d37dbd637ece9c0a63db51b0ffaed88e
marcelo paes <mpaes@ciandt.com>
2020-10-16 11:26:19 -0300

    Super com metodos - alteracao protected para private. Alteracao de bonificacao para todos os funcionarios

---
+++ java-orientado-a-objeto/bytebank-herdado/_26-Super-com-metodos/Funcionario.java
@@ -0,0 +1,43 @@
+
+public class Funcionario {
+
+	//Atributos.
+	private String nome;
+	private String cpf;
+	//protected double salario; // Publico SOMENTE PARA OS FILHOS.
+	private double salario;
+
+	//Metodo Construtor
+	public Funcionario() {
+		super();
+	}
+
+	//Metodos especiais.
+	public double getBonificacao() {
+		return this.salario * 0.05;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return this.cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getSalario() {
+		return this.salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+}
---
+++ java-orientado-a-objeto/bytebank-herdado/_26-Super-com-metodos/Gerente.java
@@ -0,0 +1,30 @@
+
+public class Gerente extends Funcionario {
+
+	// Atributos.
+	private int senha;
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	// Metodo comum.
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+//	public double getBonificacao() {
+//		return this.salario;
+//	}
+
+	// Bonificacao = bonificacao de TODOS os funcionarios + salario;
+	public double getBonificacao() {
+		//return (this.salario * 0.1) + super.salario;
+		//return (super.getBonificacao() + super.salario);
+		return (super.getBonificacao() + super.getSalario());
+	}
+}
